8d60d838f59a48293e210132425fdc0a9349500e
Ketan Bhatt <ketanbhatt@Ketans-MacBook-Pro.local>
2019-12-09 09:41:37 +0000

    Create brightness matrix

--- script.rb
+++ script.rb
@@ -4,3 +4,32 @@ image = MiniMagick::Image.open("ascii-pineapple.jpg")
 
 puts "Successfully loaded image!"
 puts "Image size: #{image.width} x #{image.height}"
+puts
+
+pixel_matrix = image.get_pixels
+
+puts "Successfully constructed pixel matrix!"
+puts "Pixel matrix size: #{pixel_matrix[0].length} x #{pixel_matrix.length}"
+puts
+
+# puts "Iterating through pixel contents:"
+# for row in pixel_matrix do
+#     for col in row do
+#         p col
+#     end
+# end
+
+brightness_matrix = Array.new
+for row in pixel_matrix do
+
+    new_col = Array.new
+    for col in row do
+        new_col.push col.reduce(:+) / col.length
+    end
+
+    brightness_matrix.push new_col
+
+end
+
+puts "Successfully constructed brightness matrix!"
+puts "Brightness matrix size: #{brightness_matrix[0].length} x #{brightness_matrix.length}"
