f24c7bf3f56e52a240e085b1dae4efcb07f40c7c
Ketan Bhatt <ketanbhatt1006@gmail.com>
2019-12-22 15:18:41 +0000

    Print somewhat identifiable ascii art

--- script.rb
+++ script.rb
@@ -1,53 +1,60 @@
 require "mini_magick"
 
-image = MiniMagick::Image.open("ascii-pineapple.jpg")
-
-puts "Successfully loaded image!"
-puts "Image size: #{image.width} x #{image.height}"
-puts
-
-pixel_matrix = image.get_pixels
-
-puts "Successfully constructed pixel matrix!"
-puts "Pixel matrix size: #{pixel_matrix[0].length} x #{pixel_matrix.length}"
-puts
-
-# puts "Iterating through pixel contents:"
-# for row in pixel_matrix do
-#     for col in row do
-#         p col
-#     end
-# end
-
-brightness_matrix = Array.new
-for row in pixel_matrix do
-    new_col = Array.new
-
-    for col in row do
-        new_col.push col.reduce(:+) / col.length
-    end
-
-    brightness_matrix.push new_col
+def load_image(img_path)
+    MiniMagick::Image.open(img_path)
 end
 
-puts "Successfully constructed brightness matrix!"
-puts "Brightness matrix size: #{brightness_matrix[0].length} x #{brightness_matrix.length}"
-puts
-
-brightness_repr_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-
-ascii_matrix = Array.new
-for row in brightness_matrix do
-    new_col = Array.new
-
-    for col in row do
-        char_idx = (col / (256 / brightness_repr_chars.length.to_f)).ceil
-        new_col.push brightness_repr_chars[char_idx]
-    end
-
-    ascii_matrix.push new_col
+def resize_image(image, new_width=100)
+    old_width, old_height = image.dimensions
+    aspect_ratio = old_width.to_f / old_height
+    new_height = (new_width / aspect_ratio).to_int
+    image.resize "#{new_width}x#{new_height}"
 end
 
-puts "Successfully constructed ASCII matrix!"
-puts "ASCII matrix size: #{ascii_matrix[0].length} x #{ascii_matrix.length}"
-puts
+def get_pixel_matrix(image)
+    image.colorspace "Gray"
+    pixel_matrix = image.get_pixels
+
+    pixel_matrix.each do |row|
+        row.map! { |col| col[0] }
+    end
+
+    pixel_matrix
+end
+
+def generate_ascii_matrix(pixel_matrix)
+    # brightness_repr_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+    brightness_repr_chars = ".,:;Il!#&8%@$"
+    # brightness_repr_chars = ['.',',',':',';','+','*','?','%','S','#', '8', '@', '$']
+    brightness_repr_chars.reverse!
+
+    pixel_matrix.each do |row|
+        row.map! do |pixel|
+            bucket = (pixel / (256 / brightness_repr_chars.length.to_f)).ceil
+            brightness_repr_chars[bucket]
+        end
+    end
+
+    pixel_matrix
+end
+
+def print_ascii_matrix(ascii_matrix)
+    for row in ascii_matrix do
+        for col in row do
+            print "#{col}"
+        end
+        puts
+    end
+end
+
+
+def asciify(img_path)
+    image = load_image(img_path)
+    resize_image(image)
+    pixel_matrix = get_pixel_matrix(image)
+    ascii_matrix = generate_ascii_matrix(pixel_matrix)
+    print_ascii_matrix(ascii_matrix)
+end
+
+# asciify("ascii-pineapple.jpg")
+asciify("octocat.png")
